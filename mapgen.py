3c25bb34772540c721ea1fc4f0a0c658a245a474
alice <aliceray678@gmail.com>
2026-07-06 21:59:09 +0100

    Added timelapse video and code generetes a gradient for now

--- mapgen.py
+++ mapgen.py
@@ -1,7 +1,12 @@
+import subprocess
+
 import numpy as np
 from PIL import Image
 import yaml
+import os
+import cv2
 
+IMAGE_PATH = 'images/'
 
 def main():
     config = yaml.safe_load(open("config.yaml"))
@@ -10,15 +15,49 @@ def main():
     size_x = config["size"]["x"]
     size_y = config["size"]["y"]
 
-    array = np.zeros((size_x, size_y, 3))
+    array = np.zeros((size_y, size_x, 3), dtype=np.uint8) # y rows and x columns, 3 channels for RGB
 
-    array = np.array(array, dtype=np.uint8)
+    for x in range(size_x):
+        for y in range(size_y):
+            # Just make a nice gradient for now
+            array[y, x] = [255*x//size_x,255*(size_y-y)//size_y,0] 
+        if x % config["freq"] == 0:
+            # Save 
+            new_image = Image.fromarray(array)
+            new_image.save(f"{IMAGE_PATH}{x}.png")
 
-    array[0,0] = [254,0,0]       # Makes the middle pixel red
-    array[1,0] = [0,0,255]       # Makes the next pixel blue
+    final = Image.fromarray(array)
+    final.save("final.png")
+    
+    generate_video(config)
+    
 
-    new_image = Image.fromarray(array)
-    new_image.save('new.png')
+
+def generate_video(config):
+    image_folder = IMAGE_PATH
+    video_name = 'timelapse.avi'
+
+    images = [img for img in os.listdir(image_folder) if img.endswith((".jpg", ".jpeg", ".png"))]
+    images.sort(key=lambda x: int(os.path.splitext(x)[0]))  # Sort images by filename (assuming they are named as numbers)
+    print("Images:", images)
+
+    # Set frame from the first image
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
+
+    # Video writer to create .avi file
+    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'DIVX'), config["fps"], (width, height))
+
+    # Appending images to video
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
+
+    # Release the video file
+    video.release()
+    cv2.destroyAllWindows()
+    print("Video generated successfully!")
+
+    subprocess.Popen(["/mnt/c/Program Files/VideoLAN/VLC/vlc.exe", video_name])
 
 
 if __name__ == '__main__':
